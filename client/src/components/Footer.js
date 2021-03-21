072e996b2ccf8c454394713751cde9130265966e
Danielby13 <44667895+Danielby13@users.noreply.github.com>
2021-03-21 19:47:07 +0200

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Segment, Container } from "semantic-ui-react";
 const Footer = () => (
   <Segment>
     <Container text textAlign="center">
-      <b>Created by:</b> Vladis, Daniel, Sapir, Lihi and Inbar 📊 📈 📉
+      <b>Created by:</b> Vladis, Daniel, Sapir, Lihi and Inbal 📊 📈 📉
     </Container>
   </Segment>
 );
