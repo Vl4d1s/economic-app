d789d46fe87c6fc238840287b4f980049724d85e
Vladis <vladismarkin@gmail.com>
2021-03-23 01:37:46 +0200

    bug fixing

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import connectDB from './config/db';
-import { APIPath } from '../configuration/index';
+import { APIPath, serverAPIPort } from '../configuration/index';
 import workers from './data/workers.json';
 import interestRate from './data/interestRate.json';
 import leavingProb from './data/leavingProb.json';
 import lifeTableWomens from './data/lifeTableWomens.json';
 import lifeTableMens from './data/lifeTableMens.json';
-const PORT = process.env.PORT || 5000;
-// console.log('starting server', { serverAPIPort, APIPath });
+// const PORT = process.env.PORT || 5000;
+console.log('starting server', { serverAPIPort, APIPath });
 
 const app = express();
 
@@ -46,10 +46,10 @@ app.get(`${APIPath}/leavingprob`, (req, res) => {
   res.status(200).send({ data: leavingProb });
 });
 
-app.listen(PORT, err => {
+app.listen(serverAPIPort, err => {
   if (err) {
     console.log(`Error: ${err.message}`);
   } else {
-    console.log(`server runnig on port ${PORT}`);
+    console.log(`server runnig on port ${serverAPIPort}`);
   }
 });
