9a7071ae7b38c9d465daaf414fa122b9b08b0e5d
Vladis <vladismarkin@gmail.com>
2021-03-28 18:33:14 +0300

    changing to heroku server

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,11 @@
-import { APIRootPath } from '@vl4d1s/conf';
+// import { APIRootPath } from "@vl4d1s/conf";
 const axios = require('axios');
+const herokuServerPath = 'https://economic-app-server.herokuapp.com/api/economic';
 
 export const createApiClient = route => {
   return {
     getData: async route => {
-      const res = await axios.get(`${APIRootPath}/${route}`);
+      const res = await axios.get(`${herokuServerPath}/${route}`);
       return res.data.data;
     },
   };
