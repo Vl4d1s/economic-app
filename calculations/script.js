bff258f3faba7f944679299db0cf9caf043e5dec
Vladis <vladismarkin@gmail.com>
2021-04-03 14:54:38 +0300

    cleaning the formula

--- calculations/script.js
+++ calculations/script.js
@@ -1,14 +1,11 @@
-const workers = require("./workers.json");
-const lifeTableWomens = require("./lifeTableWomens.json");
-const lifeTableMens = require("./lifeTableMens.json");
-const leavingProb = require("./leavingProb.json");
-const interestRate = require("./interestRate.json");
+const workers = require('./workers.json');
+const lifeTableWomens = require('./lifeTableWomens.json');
+const lifeTableMens = require('./lifeTableMens.json');
+const leavingProb = require('./leavingProb.json');
+const interestRate = require('./interestRate.json');
 
 const getDateDifference = (fistDate, secondDate) =>
-  Math.floor(
-    (new Date(fistDate) - new Date(secondDate.split("-").join(",")).getTime()) /
-      3.15576e10
-  );
+  Math.floor((new Date(fistDate) - new Date(secondDate.split('-').join(',')).getTime()) / 3.15576e10);
 
 // {
 //   id: '4',
@@ -26,10 +23,10 @@ const getDateDifference = (fistDate, secondDate) =>
 //   payProp: '0',
 //   compCheck: '0'
 // }
-const x = "2020,12,31";
+const x = '2020,12,31';
 const worker = workers[3]; // no art14 - 151,583
 const age = getDateDifference(x, worker.birthDate); // 54
-const w = workers[3] === "F" ? 64 : 67; // 64
+const w = workers[3] === 'F' ? 64 : 67; // 64
 const seniority = getDateDifference(x, worker.startJobDate); // 18
 workers[3].art14Percent = 0; // changin '' to 0 for the formula.
 const salaryGrowthRate = 0.2; // constant given by Anna.
@@ -42,20 +39,16 @@ let sum2 = 0;
 let sum3 = 0;
 
 for (let t = 0; t <= retAge; t++) {
-  var disProb = leavingProb[age + t + 1];
-  var Px =
-    t === 0
-      ? 1
-      : 1 - parseFloat(disProb.dismissal) - parseFloat(disProb.resignation);
-  var Qx = parseFloat(disProb.resignation);
-  var numerator = Math.pow(1 + salaryGrowthRate, t + 0.5) * Px * Qx;
-  var denominator = Math.pow(
-    1 + parseFloat(interestRate[t + 1].discountRate),
-    t + 0.5
-  );
+  const disProb = leavingProb[age + t + 1];
+
+  const dismissalProb = parseFloat(disProb.dismissal);
+  const resignationProb = parseFloat(disProb.resignation);
+  const discountRate = parseFloat(interestRate[t + 1].discountRate);
+
+  const Px = t === 0 ? 1 : 1 - dismissalProb - resignationProb;
+  const Qx = resignationProb;
+  const numerator = Math.pow(1 + salaryGrowthRate, t + 0.5) * Px * Qx;
+  const denominator = Math.pow(1 + discountRate, t + 0.5);
+
   sum1 = firstCalculation * (numerator / denominator);
 }
-
-console.log(
-  `sum1: ${sum1} firstCalculation:${firstCalculation} numerator:${numerator} denominator:${denominator}`
-);
